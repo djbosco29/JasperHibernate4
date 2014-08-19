e336aabcbdb433c6042a35c4cb6770192d67947e
djbosco29 <djbosco29@yahoo.com.ar>
2014-08-18 22:00:49 -0300

    Comments added
    Comments added

--- com/redb/utils/report/jasper/hibernate4/JRHibernate4QueryExecuterFactory.groovy
+++ com/redb/utils/report/jasper/hibernate4/JRHibernate4QueryExecuterFactory.groovy
@@ -1,4 +1,30 @@
-package com.redb.utils.report.jasper.hibernate4
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * This file was modified by Dario Bosco (djbosco29@redbsistemas.com)
+ * For adding Hibernate4 support.
+ */
+ package com.redb.utils.report.jasper.hibernate4
 
 import net.sf.jasperreports.engine.JRDataset
 import net.sf.jasperreports.engine.JRException
